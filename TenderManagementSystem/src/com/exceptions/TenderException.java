027c66bad2fb917ade780aaf580ceb851f980cc5
Vaishnavi <vaishnaviagrawal2999@gmail.com>
2022-12-16 11:54:41 +0530

    exceptions

---
+++ TenderManagementSystem/src/com/exceptions/TenderException.java
@@ -0,0 +1,14 @@
+package com.exceptions;
+
+public class TenderException extends Exception {
+	
+	public TenderException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public TenderException(String message) {
+		super(message);
+	}
+	
+
+}
---
+++ TenderManagementSystem/src/com/exceptions/VendorException.java
@@ -0,0 +1,15 @@
+package com.exceptions;
+
+public class VendorException extends Exception {
+	
+	public VendorException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public VendorException(String message) {
+		super(message);
+	}
+	
+
+}
+
